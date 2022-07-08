46a348ec8887070d119a6ea15c818f26234fb362
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-07-08 18:36:13 +0530

    added comments to User datastruct

--- server/internal/datastruct/user.go
+++ server/internal/datastruct/user.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 )
 
+// The User struct defines User Model
 type User struct {
 	ID       string  `json:"id"`
 	Name     string  `json:"name"`
@@ -12,6 +13,8 @@ type User struct {
 	Balance  float64 `json:"balance"`
 }
 
+// The NewUser takes id, name, email, password, balance as param
+// It returns a new User struct
 func NewUser(Id, name, email, password string, balance float64) *User {
 	return &User{
 		ID:       Id,
@@ -22,6 +25,7 @@ func NewUser(Id, name, email, password string, balance float64) *User {
 	}
 }
 
+// ToJSON method is available on User struct which marshals it to json
 func (user *User) ToJSON() (string, error) {
 	uBytes, err := json.Marshal(user)
 	if err != nil {
@@ -30,6 +34,7 @@ func (user *User) ToJSON() (string, error) {
 	return string(uBytes), nil
 }
 
+// Unmarshal method is available on User struct which unmarshal the provided data to the User struct
 func (user *User) Unmarshal(data string) error {
 	return json.Unmarshal([]byte(data), user)
 }
