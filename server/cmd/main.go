2407253c0f9ff53af4f22e3fbe79f351c2846b5b
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-07-15 18:59:54 +0530

    added grpc server

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"context"
 	"log"
+	"net"
 	"os"
 
+	"github.com/NikhilSharma03/Okane/server/internal/app"
 	"github.com/NikhilSharma03/Okane/server/internal/repository"
 	"github.com/NikhilSharma03/Okane/server/internal/service"
+	"github.com/NikhilSharma03/Okane/server/pkg/okanepb"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -18,7 +22,7 @@ func main() {
 	// Check if DB is connected
 	_, err := dbClient.Ping(context.Background()).Result()
 	if err != nil {
-		lg.Printf("Failed to connect DB. %+v", err.Error())
+		log.Fatalln("Failed to connect DB.", err)
 	}
 
 	// Initialize DAO
@@ -26,4 +30,18 @@ func main() {
 	// Register all services
 	userService := service.NewUserService(dao, lg)
 
+	// Starting gRPC server
+	go func() {
+		listener, err := net.Listen("tcp", "localhost:8001")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		grpcServer := grpc.NewServer()
+		okanepb.RegisterOkaneUserServer(grpcServer, app.NewUserService(userService))
+		err = grpcServer.Serve(listener)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
 }
