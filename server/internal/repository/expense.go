84490e7a608d3470d2b40b115821cf61340c1183
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-07-28 19:36:08 +0530

    added GetExpenses

--- server/internal/repository/expense.go
+++ server/internal/repository/expense.go
@@ -12,9 +12,8 @@ import (
 // The ExpenseCollection defines the methods a struct need to have
 type ExpenseCollection interface {
 	CreateExpense(expense_data *datastruct.Expense) (*datastruct.Expense, error)
-	GetExpense(userID string) ([]*datastruct.Expense, error)
+	GetExpenses(userID string) ([]*datastruct.Expense, error)
 	GetExpenseByID(expenseID string) (*datastruct.Expense, error)
-	UpdateExpenseByID(expense_data *datastruct.Expense) (*datastruct.Expense, error)
 	DeleteExpenseByID(expenseID string) (*datastruct.Expense, error)
 }
 
@@ -42,7 +41,37 @@ func (*expenseCollection) CreateExpense(expense_data *datastruct.Expense) (*data
 	return expense_data, nil
 }
 
-func (*expenseCollection) GetExpense(userID string) ([]*datastruct.Expense, error)
+func (*expenseCollection) GetExpenses(userID string) ([]*datastruct.Expense, error) {
+	// The result userExpenses
+	userExpenses := []*datastruct.Expense{}
+	// Get all the expense ID's of expenses created by provided userID
+	expenseIDs, err := DB.ZRevRange(context.Background(), USER_EXPENSE+userID, 0, -1).Result()
+	if err != nil {
+		if err == redis.Nil {
+			return nil, fmt.Errorf("no expense ids exists with provided userID")
+		}
+		return nil, fmt.Errorf("failed to fetch expense ids")
+	}
+	for _, expenseID := range expenseIDs {
+		// Find Expense using the expenseID
+		expString, err := DB.HGet(context.Background(), EXPENSE_COLLECTION, EXPENSE+expenseID).Result()
+		if err != nil {
+			if err == redis.Nil {
+				return nil, fmt.Errorf("no expense exists with provided expenseID")
+			}
+			return nil, fmt.Errorf("failed to fetch expense with provided ids")
+		}
+		// Unmarshal to expense
+		var expense datastruct.Expense
+		err = expense.Unmarshal(expString)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal expense data")
+		}
+		// Store expense in userExpenses
+		userExpenses = append(userExpenses, &expense)
+	}
+	return userExpenses, nil
+}
+
 func (*expenseCollection) GetExpenseByID(expenseID string) (*datastruct.Expense, error)
-func (*expenseCollection) UpdateExpenseByID(expense_data *datastruct.Expense) (*datastruct.Expense, error)
 func (*expenseCollection) DeleteExpenseByID(expenseID string) (*datastruct.Expense, error)
