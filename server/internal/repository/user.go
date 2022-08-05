f3b565068aac0c6be4a5df8b0b34f1f79aa642bf
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-08-05 16:39:25 +0530

    fixed update balance issue

--- server/internal/repository/user.go
+++ server/internal/repository/user.go
@@ -99,14 +99,67 @@ func (ur *userCollection) UpdateUserBalance(email string, expenseAmountUnits int
 	var resultBalanceUnits int64
 	var resultBalanceNanos int64
 	if expenseType == datastruct.CREDIT {
-		resultBalanceUnits = int64(user.Balance.Units + expenseAmountUnits)
-		resultBalanceNanos = int64(user.Balance.Nanos + expenseAmountNanos)
-		if resultBalanceNanos > MAX_NANOS {
-			resultBalanceUnits += 1
-			nanoString := strconv.Itoa(int(resultBalanceNanos))
-			newNanoString := nanoString[1:]
-			newNanoInt, _ := strconv.Atoi(newNanoString)
-			resultBalanceNanos = int64(newNanoInt)
+		userBalUnits := strconv.Itoa(int(user.Balance.Units))
+		userBalNanos := strconv.Itoa(int(user.Balance.Nanos))
+		expenseAmtUnits := strconv.Itoa(int(expenseAmountUnits))
+		expenseAmtNanos := strconv.Itoa(int(expenseAmountNanos))
+		userBal := userBalUnits + "."
+		for i := len(userBalNanos); i < 9; i++ {
+			userBal += "0"
+		}
+		if strings.Contains(userBalNanos, "-") {
+			userBal += strings.Replace(userBalNanos, "-", "", 1)
+			if !strings.Contains(userBal, "-") {
+				temp := "-" + userBal
+				userBal = temp
+			}
+		} else {
+			userBal += userBalNanos
+		}
+		expAmt := expenseAmtUnits + "."
+		for i := len(expenseAmtNanos); i < 9; i++ {
+			expAmt += "0"
+		}
+		if strings.Contains(expenseAmtNanos, "-") {
+			expAmt += strings.Replace(expenseAmtNanos, "-", "", 1)
+			if !strings.Contains(expAmt, "-") {
+				temp := "-" + expAmt
+				expAmt = temp
+			}
+		} else {
+			expAmt += expenseAmtNanos
+		}
+		decimal.DivisionPrecision = 9
+		userBalDec, err := decimal.NewFromString(userBal)
+		if err != nil {
+			return err
+		}
+		expAmtDec, err := decimal.NewFromString(expAmt)
+		if err != nil {
+			return err
+		}
+		result := userBalDec.Add(expAmtDec).String()
+		fmt.Println("Result ====")
+		fmt.Println(result)
+		resultArr := strings.Split(result, ".")
+		// Set Nanos and Units
+		resultUnit, _ := strconv.Atoi(string(resultArr[0]))
+		resultBalanceUnits = int64(resultUnit)
+		if len(resultArr) == 2 {
+			currNanos := string(resultArr[1])
+			currLength := len(currNanos)
+			for currLength < 9 {
+				currNanos += "0"
+				currLength++
+			}
+			resultNanos, _ := strconv.Atoi(currNanos)
+			resultBalanceNanos = int64(resultNanos)
+			if strings.Contains(result, "-") {
+				temp := -(resultBalanceNanos)
+				resultBalanceNanos = temp
+			}
+		} else {
+			resultBalanceNanos = 0
 		}
 	} else if expenseType == datastruct.DEBIT {
 		userBalUnits := strconv.Itoa(int(user.Balance.Units))
